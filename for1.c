6c7d99d47d987da406ca27355b15500c4f707adc
lucasraczenskimarques-jpg <lucas.raczenski.marques@escola.pr.gov.br>
2026-04-07 08:17:45 -0300

    Add files via upload

---
+++ for1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int i;
+
+int main(){
+
+for(int i = 1 ; i <= 50 ; i++){
+    if(i %2 == 0){
+    printf("%i\n", i);
+
+
+    }else{
+
+    }
+}
+
+
+
+
+
+
+
+
+    return 0;
+}
