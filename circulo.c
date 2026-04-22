bc065853fe9ef4fa8995c9eeab1f1d340992326d
lucasraczenskimarques-jpg <lucas.raczenski.marques@escola.pr.gov.br>
2026-04-22 10:19:10 -0300

    Add files via upload
    circulo calculo

---
+++ circulo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+float raio, area;
+
+
+printf("digite o raio de um círculo para calcule a área: \n");
+scanf("%f", &raio);
+
+
+area = 3.14 * (raio * raio);
+
+printf(" a area é %.2f", area);
+
+
+
+
+
+   return 0;
+}
