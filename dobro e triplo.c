e211d429bca2b26e3da95ae68cf19a7fb433d6ac
lucasraczenskimarques-jpg <lucas.raczenski.marques@escola.pr.gov.br>
2026-04-22 10:26:08 -0300

    Add files via upload

---
+++ dobro e triplo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+int a;
+
+printf("\n\n\n");
+
+printf("digite um  numero: \n");
+scanf("%i", &a);
+ printf("o dobro de %i é %i, e o triplo é %i\n\n\n",a , a*2, a*3);
+
+
+
+   return 0;
+}
