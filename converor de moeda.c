131ed30fef92a64cf68643757f2024de615c29f4
lucasraczenskimarques-jpg <lucas.raczenski.marques@escola.pr.gov.br>
2026-04-22 10:35:20 -0300

    Add files via upload

---
+++ converor de moeda.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+float a;
+
+
+printf("digite o valor em reais: \n \n");
+scanf("%f", &a);
+
+printf("R$ %.2f esse valor representa %.2f em dolar",a , a/4,99);
+
+
+   return 0;
+}
