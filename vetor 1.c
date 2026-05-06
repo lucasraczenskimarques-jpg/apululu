e6416202fdb5a6fcbb65328d9c75b4009f621af9
lucasraczenskimarques-jpg <lucas.raczenski.marques@escola.pr.gov.br>
2026-05-06 10:31:21 -0300

    Add files via upload

---
+++ vetor 1.c
@@ -0,0 +1,138 @@
+// crie e coloque valores para 10 vetores de tamanhos diferentes.
+
+#include <stdio.h>
+int main() {
+
+int n;
+printf("Digite o tamanho do vetor A: ");
+
+scanf("%d", &n);
+
+int A[n];
+
+printf("Digite os elementos do vetor A:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &A[i]);
+
+    }
+    printf("Digite o tamanho do vetor B: ");
+n=0;
+scanf("%d", &n);
+
+int B[n];
+
+printf("Digite os elementos do vetor B:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &B[i]);
+
+    }
+        printf("Digite o tamanho do vetor C: ");
+n=0;
+scanf("%d", &n);
+
+int C[n];
+
+printf("Digite os elementos do vetor C:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &C[i]);
+
+    }
+        printf("Digite o tamanho do vetor D: ");
+n=0;
+scanf("%d", &n);
+
+int D[n];
+
+printf("Digite os elementos do vetor D:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &D[i]);
+
+    }
+        printf("Digite o tamanho do vetor E: ");
+n=0;
+scanf("%d", &n);
+
+int E[n];
+
+printf("Digite os elementos do vetor E:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &E[i]);
+
+    }
+        printf("Digite o tamanho do vetor F: ");
+n=0;
+scanf("%d", &n);
+
+int F[n];
+
+printf("Digite os elementos do vetor F:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &F[i]);
+
+    }
+        printf("Digite o tamanho do vetor G: ");
+n=0;
+scanf("%d", &n);
+
+int G[n];
+
+printf("Digite os elementos do vetor G:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &G[i]);
+
+    }
+        printf("Digite o tamanho do vetor H: ");
+n=0;
+scanf("%d", &n);
+
+int H[n];
+
+printf("Digite os elementos do vetor H:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &H[i]);
+
+    }
+        printf("Digite o tamanho do vetor I: ");
+n=0;
+scanf("%d", &n);
+
+int I[n];
+
+printf("Digite os elementos do vetor I:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &I[i]);
+
+    }
+        printf("Digite o tamanho do vetor J: ");
+n=0;
+scanf("%d", &n);
+
+int J[n];
+
+printf("Digite os elementos do vetor J:\n");
+
+for(int i = 0; i < n; i++) {
+
+scanf("%d", &J[i]);
+
+    }
+    return 0;
+}
